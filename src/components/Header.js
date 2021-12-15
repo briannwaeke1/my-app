efcf704602af5fcd1f91450e66d0ee423e2aa782
Brian Nwaeke <bnwaeke@icloud.com>
2021-12-14 22:33:18 -0800

    header component and logo creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from "../logo.png";
+import logo from "../logoTran.png";
+import '../App.css';
 
 export const Header = () => {
     return (
-      <div className="header-wraper">
-        <img className='logo' src={logo} alt='logo'/>
+      <div className="headerItem text-center">
+      <img src={logo} className='logo' alt='logo' />
       </div>
     )
   }
   
+  
+
   
